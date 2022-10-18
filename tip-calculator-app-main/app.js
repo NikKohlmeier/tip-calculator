c9888e57fb0f8f7808cd613ca86004fb3c6843a0
NikKohlmeier <63984312+NikKohlmeier@users.noreply.github.com>
2022-10-18 11:33:01 -0400

    WIP-HTML & CSS
    No mobile yet

---
+++ tip-calculator-app-main/app.js
@@ -0,0 +1,8 @@
+const billInput = document.querySelector("#bill-input");
+const peopleInput = document.querySelector("#people-input")
+
+const tipButton5 = document.querySelector("#tip-btn-5");
+const tipButton10 = document.querySelector("#tip-btn-10");
+const tipButton15 = document.querySelector("#tip-btn-15");
+const tipButton25 = document.querySelector("#tip-btn-25");
+const tipButton50 = document.querySelector("#tip-btn-50");
